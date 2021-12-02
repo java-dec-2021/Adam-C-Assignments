829a243665b9360759f0a9b4b9d4df31d8e8621c
Adam Can <adam.inthemorning@gmail.com>
2021-12-02 12:08:29 -0500

    BasicJava bonus sections added.

--- 01-java-fundamentals/05-basics/BasicJava.java
+++ 01-java-fundamentals/05-basics/BasicJava.java
@@ -28,8 +28,8 @@ public class BasicJava {
 
     // Iterate over a given array & print each value to screen
     public void printArrayValues(int[] arr) {
-        for (int i=0;i<arr.length;i++) {
-            System.out.println(arr[i]);
+        for (int i : arr) {
+            System.out.println(i);
         }
     }
 
@@ -52,5 +52,80 @@ public class BasicJava {
             }
         }
         return numbers;
+    }
+
+
+    // NINJA BONUS!
+    // Get the average of a given array of ints
+    public double getAverage(int[] arr) {
+        double sum = 0.0;
+        double avg = 0.0;
+        for (int i=0;i<arr.length;i++) {
+            sum += arr[i];
+        }
+        avg = (sum / arr.length);
+        return avg;
+    }
+
+    // Get number of values in given int array that are greater than a given int
+    public int countGreaterThanY(int[] arr, int y) {
+        int count =0;
+        for (int i=0;i<arr.length;i++) {
+            if (arr[i]>y) {
+                count+=1;
+            }
+        }
+        return count;
+    }
+
+    // Square each value within a given int array\
+    public int[] squareValues(int[] arr) {
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = arr[i]*arr[i];
+        }
+        return arr;
+    }
+
+    // Eliminate all negative numbers in int array (replace them with 0)
+    public int[] removeNegatives(int[] arr) {
+        for (int i=0;i<arr.length;i++) {
+            if (arr[i]<0) {
+                arr[i]=0;
+            }
+        }
+        return arr;
+    }
+
+
+    // SENSEI BONUS!!
+    // Return an array containing [max,min,avg] values from a given int array
+    public Object[] grabArrayInfo(int[] arr) {
+        Object[] arrayInfo = new Object[3];
+        int max = arr[0];
+        int min = arr[0];
+        for (int i=0;i<arr.length;i++) {
+            if (max<arr[i]) {
+                max = arr[i];
+            }
+            else if (min>arr[i]) {
+                min = arr[i];
+            }
+        }
+        arrayInfo[0] = max;
+        arrayInfo[1] = min;
+        arrayInfo[2] = getAverage(arr);
+        return arrayInfo;
+    }
+
+    // Shift the values in the array to the left.
+    public int[] shiftValuesLeft(int[] arr) {
+        for (int i=0;i<arr.length;i++) {
+            if (i<arr.length-1) {
+                arr[i] = arr[i+1];
+            } else {
+                arr[i] = 0;
+            }
+        }
+        return arr;
     }
 }
--- 01-java-fundamentals/05-basics/TestBasicJava.java
+++ 01-java-fundamentals/05-basics/TestBasicJava.java
@@ -6,12 +6,35 @@ public class TestBasicJava {
 
         //javaCode.printNumbers();
         //javaCode.printOddNumbers();
-        //int totalSum = javaCode.sumTo255();
-        //System.out.println(totalSum);
-        int[] intArray = new int[]{1,7,2,-5,10,23,28,-13,-7,0};
+        // int totalSum = javaCode.sumTo255();
+        // System.out.println(totalSum);
+        int[] intArray = new int[]{1,7,2,-5,10,-8,22,-13,-7,5};
         //javaCode.printArrayValues(intArray);
         //javaCode.printMaxValue(intArray);
         ArrayList<Integer> oddNumbers255 = javaCode.createAnArray();
         //System.out.println(oddNumbers255);
-    }
+        
+
+        // NINJA BONUS
+        
+        //double average = javaCode.getAverage(intArray);
+        //System.out.println(average);
+        
+        //int countGreaterThanY = javaCode.countGreaterThanY(intArray,7);
+        //System.out.println(countGreaterThanY);
+        
+        //int[] squareArray = javaCode.squareValues(intArray);
+        //javaCode.printArrayValues(squareArray);
+        
+        //int[] arrNoNeg = javaCode.removeNegatives(intArray);
+        //javaCode.printArrayValues(arrNoNeg);
+        
+        // Object[] arrayMaxMinAvg = javaCode.grabArrayInfo(intArray);
+        // for (Object x : arrayMaxMinAvg) {
+        //     System.out.println(x);
+        // }
+
+        // int[] arrayShiftedLeft = javaCode.shiftValuesLeft(intArray);
+        // javaCode.printArrayValues(arrayShiftedLeft);
+        }
 }
